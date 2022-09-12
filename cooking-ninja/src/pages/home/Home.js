721d097cdf904d7eca2a3d3b9bb00ecea85eefe9
AshFoster <ashfoster87@gmail.com>
2022-09-12 16:21:06 +0000

    Add useFetch hook and output recipe titles on Home page

--- cooking-ninja/src/pages/home/Home.js
+++ cooking-ninja/src/pages/home/Home.js
@@ -1,10 +1,18 @@
+import { useFetch } from '../../hooks/useFetch' 
+
 // styles
 import './Home.css'
 
 export default function Home() {
+  const { data, isPending, error } = useFetch('https://3000-ashfoster-cookingninja-ic7h8hmnlqs.ws-eu64.gitpod.io/recipes')
+
   return (
-    <div>
-        Home
+    <div className='home'>
+        {error && <p className='error'>{error}</p>}
+        {isPending && <p className='loading'>Loading...</p>}
+        {data && data.map(recipe => (
+          <h2 key={recipe.id}>{recipe.title}</h2>
+        ))}
     </div>
   )
 }
