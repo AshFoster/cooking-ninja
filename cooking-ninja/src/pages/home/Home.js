c153effca0c217e2cb62a3cd14656486472839df
AshFoster <ashfoster87@gmail.com>
2022-09-14 13:55:38 +0000

    Add RecipeList component

--- cooking-ninja/src/pages/home/Home.js
+++ cooking-ninja/src/pages/home/Home.js
@@ -3,16 +3,17 @@ import { useFetch } from '../../hooks/useFetch'
 // styles
 import './Home.css'
 
+// components
+import RecipeList from '../../components/RecipeList'
+
 export default function Home() {
-  const { data, isPending, error } = useFetch('https://3000-ashfoster-cookingninja-ic7h8hmnlqs.ws-eu64.gitpod.io/recipes')
+  const { data, isPending, error } = useFetch('https://3000-ashfoster-cookingninja-ra8jqzxyfw4.ws-eu64.gitpod.io/recipes')
 
   return (
     <div className='home'>
         {error && <p className='error'>{error}</p>}
         {isPending && <p className='loading'>Loading...</p>}
-        {data && data.map(recipe => (
-          <h2 key={recipe.id}>{recipe.title}</h2>
-        ))}
+        {data && <RecipeList recipes={data} />}
     </div>
   )
 }
